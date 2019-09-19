b91c9fec08e2b41c956f533f3dd2a3475d95ee2f
Erigleydson Omena <erigleydson.tj@gmail.com>
2019-09-19 00:14:15 -0300

    jogo alura

---
+++ jogo.rb
@@ -0,0 +1,48 @@
+def da_boas_vindas
+	puts "Bem-vindo ao jogo da adivinhação"
+	puts "Qual é o seu nome?"
+	nome = gets
+	puts "\n\n\n\n\n"
+	puts "Começaremos o jogo para você, " + nome
+end
+
+def sorteia_numero_secreto
+	puts "Escolhendo um número secreto entre 0 e 200..."
+	sorteado = 175
+	puts "Escolhido... que tal adivinhar hoje nosso número secreto?"
+	sorteado
+end
+
+def pede_um_numero(tentativa, limite_de_tentativas)
+	puts "\n\n\n\n"
+	puts "Tentativa " + tentativa.to_s + " de " + limite_de_tentativas.to_s
+	puts "Entre com o número"
+	chute = gets
+	puts "Será que acertou? Você chutou " + chute
+	chute.to_i
+end
+
+da_boas_vindas
+numero_secreto = sorteia_numero_secreto
+
+limite_de_tentativas = 5
+
+for tentativa in 1..limite_de_tentativas
+
+	chute = pede_um_numero(tentativa, limite_de_tentativas)
+
+	acertou = numero_secreto == chute
+
+	if acertou
+		puts 'Acertou!!'
+		break
+	else
+		maior = numero_secreto > chute
+
+		if maior
+			puts "O número é maior"
+		else
+			puts 'O número é menor'
+		end
+	end
+end
